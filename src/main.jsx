21263f0b890917b1784279a565d2b43769493f2c
iden0605 <79390850+iden0605@users.noreply.github.com>
2025-08-31 05:56:44 +1000

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+document.body.addEventListener("touchmove", function (e) {
+      e.preventDefault();
+});
+document.body.addEventListener("scroll", function (e) {
+      e.preventDefault();
+});
+document.body.addEventListener("wheel",function (e) {
+      e.preventDefault();
+}, false);
